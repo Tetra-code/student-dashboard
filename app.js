7920be5e1c328a89a1a426e8d2f7ccb17414ad75
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-04-23 13:58:08 +0200

    login function for user accounts

--- app.js
+++ app.js
@@ -1,44 +1,95 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
-const ws = require("ws")
+const ws = require("ws");
 const app = express();
+const bcrypt = require("bcrypt");
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+const users =[];
+
+const initializePassport = require('./passport-config');
+initializePassport(passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+);
+
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "ejs");
-app.use(express.static(`${__dirname}/public`, {
-    extensions:['html']
-}));
+app.use(
+    express.static(`${__dirname}/public`), 
+    express.urlencoded({ extended: false}),
+    flash(),
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    }),
+    passport.initialize(),
+    passport.session(),
+    methodOverride('_method')
+)
 
-app.get('/', (req, res) => {
+app.get('/', checkNotAuthenticated, (req, res) => {
     res.render("splash");
 })
 
-app.get('/home', (req, res) => {
-    res.render("home");
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render("login");
 })
 
-app.get('/calendar', (req, res) => {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render("register");
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register')
+    }
+});
+
+app.delete('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
+
+app.get('/home', checkAuthenticated, (req, res) => {
+    res.render("home", {name: req.user.username});
+});
+
+app.get('/calendar', checkAuthenticated, (req, res) => {
     res.render("calendar");
-})
+});
 
-app.get('/timeline', (req, res) => {
+app.get('/timeline', checkAuthenticated, (req, res) => {
     res.render("timeline");
-})
+});
 
-app.get('/settings', (req, res) => {
+app.get('/settings', checkAuthenticated, (req, res) => {
     res.render("settings");
-})
-
-const users ={};
-//guid function for clientId
-const guid = function() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-}
-
+});
 
 const server = http.createServer(app);
 const wss = new ws.Server({ server });
@@ -53,6 +104,20 @@ wss.on("connection", (con) => {
     con.on("close", () => {
     })
 });
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/home')
+    }
+    next();
+}
 
 
 server.listen(port, ()=>{
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,12 @@
 const wsProtocol = location.protocol.includes('https') ? 'wss:' : 'ws:';
 const socket = new WebSocket(`${wsProtocol}//${location.host}`);
 
+// const guid = function() {
+//     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+//         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+//         return v.toString(16);
+//     });
+// }
 
 socket.onmessage = (message) =>{
     
